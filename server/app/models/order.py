030c118dda54b635524a146722b58d9a6e770230
hmlucas <124640713+hmlucas@users.noreply.github.com>
2024-10-23 12:19:28 -0500

    order (w/o tickets)

---
+++ server/app/models/order.py
@@ -0,0 +1,23 @@
+from app.extensions import db
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    order_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    payment_method = db.Column(db.String(32), nullable=False)
+    order_date = db.Column(db.Date, nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    
+
+    def __repr__(self):
+        return f"<Order {self.order_id}>"
+
+    def to_dict(self):
+        return {
+            "order_id": self.order_id,
+            "payment_method": self.payment_method,
+            "order_date": self.order_date.isoformat(),
+            "employee_id": self.employee_id,
+            "price": self.price
+        }
