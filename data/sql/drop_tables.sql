b9f5809a77d31f17dcb076337eaf0934adde32cf
gmgrahamgm <grahamd@tamu.edu>
2024-10-24 17:10:27 -0500

    Added old SQL scripts

---
+++ data/sql/drop_tables.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS menuitem_options_join, options_ingredients_join, menu_items, orders, employees, ingredients, options;
+
---
+++ data/sql/empty_tables.sql
@@ -0,0 +1,10 @@
+TRUNCATE TABLE
+    options_ingredients_join,
+    menuitem_options_join,
+    menu_items,
+    orders,
+    employees,
+    options,
+    ingredients
+CASCADE;
+
