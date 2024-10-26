1c527d87d68d0e6ed1004d0873241f12290d6841
CMBCAM <cameron.barradas@gmail.com>
2024-10-26 00:20:34 -0500

    Added options and request by category

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -1,4 +1,5 @@
 from .employees import employees_bp
+from .options import options_bp
 from .orders import orders_bp
 from .menu_items import menu_items_bp
 from .ingredients import ingredients_bp
@@ -6,5 +7,6 @@ from .ingredients import ingredients_bp
 def register_blueprints(app):
     app.register_blueprint(employees_bp, url_prefix='/api/employees')
     app.register_blueprint(orders_bp, url_prefix='/api/orders') 
+    app.register_blueprint(options_bp, url_prefix='/api/option') 
     app.register_blueprint(menu_items_bp, url_prefix='/api/menu_items') 
     app.register_blueprint(ingredients_bp, url_prefix='/api/ingredients') 
