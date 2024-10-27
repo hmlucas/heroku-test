27c9bf12467719b4c62bfa4a201aa924ea846e9a
hmlucas <124640713+hmlucas@users.noreply.github.com>
2024-10-27 01:03:56 -0500

    wrong about more models. but does provide menu item options from an order yay!!

--- server/app/api/orders.py
+++ server/app/api/orders.py
@@ -11,3 +11,14 @@ def get_orders():
 def get_order(order_id):
     order = OrderRepository.get_order_or_404(order_id)
     return jsonify(order.to_dict()), 200
+@orders_bp.route('/<int:order_id>/mi', methods=['GET'])
+def get_order_menu_items(order_id):
+    menu_items = OrderRepository.get_order_menu_items(order_id)
+    menu_item_options = [
+        {
+            "menu_item_id": menu_item.menuitem_id,
+            "options": [option.option for option in menu_item.options] 
+        }
+        for menu_item in menu_items
+    ]
+    return jsonify(menu_item_options), 200
--- server/app/repos/order_repository.py
+++ server/app/repos/order_repository.py
@@ -1,6 +1,8 @@
 from ..models.order import Order
+from ..models.menu_item import MenuItem
+from ..models.options import Options
 from ..extensions import db
-from sqlalchemy import select, update
+from sqlalchemy import select, update, Table, Column, Integer, ForeignKey, String
 from flask import abort
 
 class OrderRepository:
@@ -54,6 +56,21 @@ class OrderRepository:
         db.session.add(order)
         db.session.commit()
         
-    def insert_menu_items(order):
-        #TODO insert menu items assoc w/ order
-        pass
+    def get_order_menu_items(order_id):
+        menuitem_options_join = Table(
+            'menuitem_options_join',
+            db.Model.metadata,
+            Column('menuitem_option_id', Integer, primary_key=True),
+            Column('menuitem_id', Integer, ForeignKey('menu_items.menuitem_id')),
+            Column('option', String(64), ForeignKey('options.option')),
+            extend_existing=True
+        )
+        menu_items = (
+            db.session.query(MenuItem)
+            .join(menuitem_options_join, MenuItem.menuitem_id == menuitem_options_join.c.menuitem_id)
+            .join(Options, Options.option == menuitem_options_join.c.option)
+            .filter(MenuItem.order_id == order_id)
+            .all()
+        )
+        return menu_items
+        
