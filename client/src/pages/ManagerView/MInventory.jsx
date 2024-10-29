31ab86f35edd1897da0598713f873eb4a845cdc7
HomeoStasis69 <javibetancourt2004@gmail.com>
2024-10-29 14:49:30 -0500

    Update .gitignore to ignore launch.json and clear cache

--- client/src/pages/ManagerView/MInventory.jsx
+++
@@ -1,5 +0,0 @@
-function MInventory() {
-  return <h1>Inventory</h1>;
-}
-
-export default MInventory;
--- server/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .employee import Employee
-from .ingredient import Ingredient
-from .menu_item import MenuItem
-from .options import Options
-from .order import Order
-
-__all__ = ["Employee", "Ingredient", "MenuItem","Options", "Order"]
