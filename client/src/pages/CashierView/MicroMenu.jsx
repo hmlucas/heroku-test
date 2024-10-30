0ab35348c2e8870d6aa6ea074ff58e99da89def8
CMBCAM <cameron.barradas@gmail.com>
2024-10-30 01:23:41 -0500

    initial micro menu and states, able to change the menu upon call

--- client/src/pages/CashierView/MicroMenu.jsx
+++ client/src/pages/CashierView/MicroMenu.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
 import "./CashierView.css";
+import MicroNewItem from "./MicroMenu/MicroNewItem";
+import MicroOptions from "./MicroMenu/MicroOptions";
+import MicroSides from "./MicroMenu/MicroSides";
+import MicroEntrees from "./MicroMenu/MicroEntrees";
+import MicroDrinks from "./MicroMenu/MicroDrinks";
+import MicroCheckout from "./MicroMenu/MicroCheckout";
+import MicroAppetizers from "./MicroMenu/MicroAppetizers";
+import MicroALaCarte from "./MicroMenu/MicroALaCarte";
+import MenuEnum from "./MenuEnum";
 
 const MicroMenu = () => {
+  const [activeMenu, setActiveMenu] = useState(MenuEnum.NEW_ITEM);
+  const changeMenu = (index) => {
+    if (index in MenuEnum) {
+      setActiveMenu(index); // Update the state with the selected component
+    }
+  };
+
+  const renderMenu = () => {
+    switch (activeMenu) {
+      case MenuEnum.NEW_ITEM:
+        return <MicroNewItem />;
+      case MenuEnum.ENTREES:
+        return <MicroEntrees />;
+      case MenuEnum.SIDES:
+        return <MicroSides />;
+      case MenuEnum.DRINKS:
+        return <MicroDrinks />;
+      case MenuEnum.APPETIZERS:
+        return <MicroAppetizers />;
+      case MenuEnum.A_LA_CARTE:
+        return <MicroALaCarte />;
+      case MenuEnum.CHECKOUT:
+        return <MicroCheckout />;
+      case MenuEnum.OPTIONS:
+        return <MicroOptions />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="cashier-micro-menu">
-      <h1>MicroMenu</h1>
+      <h1>MicroMenu=</h1>
+      <div className="menu-display">
+        {renderMenu()} {/* Render the selected menu */}
+      </div>
     </div>
   );
 };
---
+++ client/src/pages/CashierView/MicroMenu/MicroAppetizers.jsx
@@ -0,0 +1,11 @@
+import "../CashierView.css";
+
+const MicroAppetizers = () => {
+  return (
+    <div className="cashier-micro-appetizers">
+      <h1>Appetizers</h1>
+    </div>
+  );
+};
+
+export default MicroAppetizers;
