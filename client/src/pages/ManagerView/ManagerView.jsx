41f28fdf8c162fc8ea5a147e6f2c55bfb85554a4
hmlucas <124640713+hmlucas@users.noreply.github.com>
2024-10-30 13:14:13 -0500

    routing in managerview

--- client/src/pages/ManagerView/ManagerView.jsx
+++ client/src/pages/ManagerView/ManagerView.jsx
@@ -1,23 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-
 import MEmployees from "./MEmployees";
 import MInventory from "./MInventory";
 import MMenuItems from "./MMenuItems";
 import MOrderTrends from "./MOrderTrends";
-
-function ManagerHome() {
-  return <h1>Manager View</h1>;
-}
+import ManagerHome from "./ManagerHome";
 
 function ManagerView() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<ManagerHome />} />
-        <Route path="/employees" element={<MEmployees />} />
-        <Route path="/inventory" element={<MInventory />} />
-        <Route path="/menu-items" element={<MMenuItems />} />
-        <Route path="/order-trends" element={<MOrderTrends />} />
+        <Route path="employees" element={<MEmployees />} />
+        <Route path="inventory" element={<MInventory />} />
+        <Route path="menu-items" element={<MMenuItems />} />
+        <Route path="order-trends" element={<MOrderTrends />} />
       </Routes>
     </div>
   );
