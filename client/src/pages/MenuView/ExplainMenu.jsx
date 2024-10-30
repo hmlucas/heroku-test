d804e4fc27b2ba42a45d10bc78e0707f130df23f
gmgrahamgm <grahamd@tamu.edu>
2024-10-30 14:11:22 -0500

    Added assets folder, updated menu view scripts

--- client/src/pages/MenuView/ExplainMenu.jsx
+++ client/src/pages/MenuView/ExplainMenu.jsx
@@ -4,24 +4,18 @@ const ExplainMenu = () => {
   return (
     <div className="menu-explain-menu">
       <div className="menu-item">
-        <img
-          src="src/Pictures/MenuViewPics/panda_bowl.jpg"
-          alt="Panda Bowl"
-        />
+        <img src="src/assets/menuview/panda_bowl.jpg" alt="Panda Bowl" />
         <p>Panda Bowl</p>
         <p>1 Side 1 Entree</p>
       </div>
       <div className="menu-item">
-        <img
-          src="src/Pictures/MenuViewPics/panda_plate.jpg"
-          alt="Panda Plate"
-        />
+        <img src="src/assets/menuview/panda_plate.jpg" alt="Panda Plate" />
         <p>Panda Plate</p>
         <p>1 Side 2 Entree</p>
       </div>
       <div className="menu-item">
         <img
-          src="src/Pictures/MenuViewPics/panda_bigger_plate.jpg"
+          src="src/assets/menuview/panda_bigger_plate.jpg"
           alt="Panda Bigger Plate"
         />
         <p>Panda Bigger Plate</p>
