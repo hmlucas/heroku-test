9bc01c9961cf83b8ea3610646bda17b0d4dc2e0c
CMBCAM <cameron.barradas@gmail.com>
2024-10-29 23:50:40 -0500

    image options for salesmenu

--- client/src/pages/CashierView/SalesMenu.jsx
+++ client/src/pages/CashierView/SalesMenu.jsx
@@ -1,15 +1,27 @@
 import "./CashierView.css";
 import "./SalesMenu.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useEmployeeStore from "../../store/useEmployeeStore";
+// import axios from "axios";
 
 function SalesMenu() {
   const { selectedEmployee, fetchEmployeeById } = useEmployeeStore();
+  // const [imageName, setImageName] = useState("./src/img/placeholder.gif");
+
+  const [hasIncompleteOrder, setHasIncompleteOrder] = useState(false);
+  const [hasTickets, setHasTickets] = useState(false);
+  const [hasSelectedOrder, setHasSelectedOrder] = useState(false);
 
   useEffect(() => {
     fetchEmployeeById(1); // fetch current active employee //TODO make this use current employee (remove the fetch?)
+
+    //TODO placeholder states - should update based on current order state
+    setHasTickets(true);
+    setHasSelectedOrder(true);
+    setHasIncompleteOrder(false);
   }, [fetchEmployeeById]);
 
+  // TODO Add click functionality
   const optionsAction = () => {
     alert("FIXME Options Clicked!");
   };
@@ -19,15 +31,30 @@ function SalesMenu() {
   const checkoutAction = () => {
     alert("FIXME Checkout Clicked!");
   };
-
+  /*
+  Sales Menu States: 
+    - Incomplete order (edit or new one) - no checkout button
+    - No tickets - no delete no checkout
+    - No selected order (recent deletion) - no delete
+*/
   return (
     <div className="cashier-sales-menu">
       <div className="cashier-sales-buttons">
         <div className="cashier-delete-button">
-          <button onClick={deleteAction}>Delete Item</button>
+          <button
+            onClick={deleteAction}
+            disabled={!hasTickets || !hasSelectedOrder}
+          >
+            Delete Item
+          </button>
         </div>
         <div className="cashier-checkout-button">
-          <button onClick={checkoutAction}>Checkout</button>
+          <button
+            onClick={checkoutAction}
+            disabled={!hasTickets || hasIncompleteOrder}
+          >
+            Checkout
+          </button>
         </div>
         <div className="cashier-employee-name">
           {selectedEmployee ? (
@@ -39,7 +66,13 @@ function SalesMenu() {
           )}
         </div>
         <div className="cashier-options-button">
-          <button onClick={optionsAction}>Exit</button>
+          <button onClick={optionsAction}>
+            <img
+              src="./src/img/placeholder.gif"
+              alt="Options"
+              className="cashier-options-image"
+            />
+          </button>
         </div>
       </div>
     </div>
