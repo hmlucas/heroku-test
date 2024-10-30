069629b522ba392ac21db8b931a6f834f094aede
CMBCAM <cameron.barradas@gmail.com>
2024-10-29 23:13:24 -0500

    basic click functions - do no lead anywhere

--- client/src/pages/CashierView/SalesMenu.jsx
+++ client/src/pages/CashierView/SalesMenu.jsx
@@ -10,14 +10,24 @@ function SalesMenu() {
     fetchEmployeeById(1); // fetch current active employee //TODO make this use current employee (remove the fetch?)
   }, [fetchEmployeeById]);
 
+  const optionsAction = () => {
+    alert("FIXME Options Clicked!");
+  };
+  const deleteAction = () => {
+    alert("FIXME Delete Clicked!");
+  };
+  const checkoutAction = () => {
+    alert("FIXME Checkout Clicked!");
+  };
+
   return (
     <div className="cashier-sales-menu">
       <div className="cashier-sales-buttons">
         <div className="cashier-delete-button">
-          <button>Delete Item</button>
+          <button onClick={deleteAction}>Delete Item</button>
         </div>
         <div className="cashier-checkout-button">
-          <button>Checkout</button>
+          <button onClick={checkoutAction}>Checkout</button>
         </div>
         <div className="cashier-employee-name">
           {selectedEmployee ? (
@@ -29,7 +39,7 @@ function SalesMenu() {
           )}
         </div>
         <div className="cashier-options-button">
-          <button>Options</button>
+          <button onClick={optionsAction}>Exit</button>
         </div>
       </div>
     </div>
