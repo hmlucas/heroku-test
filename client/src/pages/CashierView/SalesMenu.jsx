629d7fbc530fbd6439642b5d131bfbf754692eb5
CMBCAM <cameron.barradas@gmail.com>
2024-10-29 23:02:39 -0500

    sales menu buttons and employee name in sales menu , hover and resize based on widnow. they do not have any action yet. also using normal buttons

--- client/src/pages/CashierView/SalesMenu.jsx
+++ client/src/pages/CashierView/SalesMenu.jsx
@@ -1,11 +1,39 @@
 import "./CashierView.css";
+import "./SalesMenu.css";
+import { useEffect } from "react";
+import useEmployeeStore from "../../store/useEmployeeStore";
 
-const SalesMenu = () => {
+function SalesMenu() {
+  const { selectedEmployee, fetchEmployeeById } = useEmployeeStore();
+
+  useEffect(() => {
+    fetchEmployeeById(1); // fetch current active employee //TODO make this use current employee (remove the fetch?)
+  }, [fetchEmployeeById]);
+
   return (
     <div className="cashier-sales-menu">
-      <h1>SalesMenu</h1>
+      <div className="cashier-sales-buttons">
+        <div className="cashier-delete-button">
+          <button>Delete Item</button>
+        </div>
+        <div className="cashier-checkout-button">
+          <button>Checkout</button>
+        </div>
+        <div className="cashier-employee-name">
+          {selectedEmployee ? (
+            <p>
+              {selectedEmployee.first_name} {selectedEmployee.last_name}
+            </p>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+        <div className="cashier-options-button">
+          <button>Options</button>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default SalesMenu;
